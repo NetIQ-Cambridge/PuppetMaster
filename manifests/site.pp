007446d2a0c69c648930b2c137e25ec52e08af6e
root <root@ip-10-160-83-223.us-west-1.compute.internal>
2011-07-08 21:45:26 +0000

    Adjust for EC2 deployment

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,16 @@
-node default {
-	include ssh::authorizedkeys
-        include mongodb::shard 
-	include mongodb::router
+class base {
+        include mongodb::shard
+        include mongodb::router
 }
 
-node 'mongodb01.qalab.cam.novell.com' {
+class super inherits base {
         include mongodb::configsvr
+}
+
+node default {
+	include base
+}
+
+node 'ip-10-176-30-36.us-west-1.compute.internal' {
+        include super
 }
